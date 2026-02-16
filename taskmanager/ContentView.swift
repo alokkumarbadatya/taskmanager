42879f921db9424d150a267dd3b5bdd409485e8e
alok kumar badatya <alokrock19s7@gmail.com>
2026-02-16 14:22:11 +0530

    Initial Commit

---
+++ taskmanager/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  taskmanager
+//
+//  Created by alok kumar badatya on 16/02/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ taskmanager/taskmanagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  taskmanagerApp.swift
+//  taskmanager
+//
+//  Created by alok kumar badatya on 16/02/26.
+//
+
+import SwiftUI
+
+@main
+struct taskmanagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
